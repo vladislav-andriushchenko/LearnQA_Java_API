c587e388f6a4df13ffec8287bc0b13c093428acb
vladislav-andriushchenko <vladitheboy@gmail.com>
2023-12-03 04:56:14 -0800

    - Added testGetText

--- src/test/java/GreetingTest.java
+++ src/test/java/GreetingTest.java
@@ -1,4 +1,6 @@
 import org.junit.jupiter.api.Test;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 
 public class GreetingTest {
 
@@ -6,5 +8,15 @@ public class GreetingTest {
     public void testGreeting() {
         String name = "Vladislav";
         System.out.println("Hello from " + name);
+    }
+
+    @Test
+    public void testGetText() {
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_text")
+                .andReturn();
+
+        response.prettyPrint();
+
     }
 }
