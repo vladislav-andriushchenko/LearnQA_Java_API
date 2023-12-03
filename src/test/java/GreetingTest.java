926863e16c1d5b5e2fe5eee1444a7e28bd0ea0a8
vladislav-andriushchenko <vladitheboy@gmail.com>
2023-12-03 04:47:33 -0800

    - Added GreetingTest

---
+++ src/test/java/GreetingTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+public class GreetingTest {
+
+    @Test
+    public void testGreeting() {
+        String name = "Vladislav";
+        System.out.println("Hello from " + name);
+    }
+}
