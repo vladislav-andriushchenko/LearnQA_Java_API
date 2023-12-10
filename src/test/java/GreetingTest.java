63a8e749212dd98f209014df11a915dfa7d73f55
vladislav-andriushchenko <vladitheboy@gmail.com>
2023-12-10 03:24:01 -0800

    - Added getJsonHomework

--- src/test/java/GreetingTest.java
+++ src/test/java/GreetingTest.java
@@ -1,6 +1,7 @@
-import org.junit.jupiter.api.Test;
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
 
 public class GreetingTest {
 
@@ -18,5 +19,18 @@ public class GreetingTest {
 
         response.prettyPrint();
 
+    }
+
+    @Test
+    public void getJsonHomework() {
+
+        JsonPath response = RestAssured
+                .given()
+                .get("https://playground.learnqa.ru/api/get_json_homework")
+                .jsonPath();
+
+        response.prettyPrint();
+        String name = response.get("messages[1].message");
+        System.out.println(name);
     }
 }
