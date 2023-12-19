beb06ae2c1ccbc7f762e2f7949e9af289c4a247d
vladislav-andriushchenko <vladitheboy@gmail.com>
2023-12-19 10:52:51 -0800

    - Added testUserAgent

--- src/test/java/GreetingTest.java
+++ src/test/java/GreetingTest.java
@@ -3,17 +3,35 @@ import io.restassured.http.Headers;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class GreetingTest {
 
+    private static Stream<Arguments> testData() {
+        return Stream.of(
+                Arguments.of("Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30",
+                        "Mobile", "No", "Android"),
+                Arguments.of("Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1",
+                        "Mobile", "Chrome", "iOS"),
+                Arguments.of("Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)",
+                        "Googlebot", "Unknown", "Unknown"),
+                Arguments.of("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0",
+                        "Web", "Chrome", "No"),
+                Arguments.of("Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1",
+                        "Mobile", "No", "iPhone")
+        );
+    }
 
     @Test
     public void testGreeting() {
@@ -218,5 +236,26 @@ public class GreetingTest {
         assertEquals(headers.getValue("x-secret-homework-header"), "Some secret value",
                 "The header doesn't have 'Some secret value' value: " + headers.getValue("x-secret-homework-header"));
 
+    }
+
+    @ParameterizedTest
+    @MethodSource("testData")
+    public void testUserAgent(String userAgent, String expectedPlatform, String expectedBrowser, String expectedDevice) {
+        String url = "https://playground.learnqa.ru/ajax/api/user_agent_check";
+
+        JsonPath response = RestAssured
+                .given()
+                .header("User-Agent", userAgent)
+                .when()
+                .get(url)
+                .jsonPath();
+
+        String actualPlatform = response.get("platform");
+        String actualBrowser = response.get("browser");
+        String actualDevice = response.get("device");
+
+        assertEquals(expectedPlatform, actualPlatform, "Response doesn't have a required Platform");
+        assertEquals(expectedBrowser, actualBrowser, "Response doesn't have a required Browser");
+        assertEquals(expectedDevice, actualDevice, "Response doesn't have a required Device");
     }
 }
