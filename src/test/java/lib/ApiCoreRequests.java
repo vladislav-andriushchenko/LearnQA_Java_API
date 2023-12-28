e1860446c08684214992a8f629eb4a07587c1ab8
vladislav-andriushchenko <vladitheboy@gmail.com>
2023-12-28 05:44:15 -0800

    - Added new tests to UserEditTest

--- src/test/java/lib/ApiCoreRequests.java
+++ src/test/java/lib/ApiCoreRequests.java
@@ -6,6 +6,7 @@ import io.restassured.http.Header;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import static io.restassured.RestAssured.given;
@@ -55,5 +56,56 @@ public class ApiCoreRequests {
                 .body(authData)
                 .post(url)
                 .jsonPath();
+    }
+
+    @Step("Make a PUT-request")
+    public Response makePutRequest(String url, Map<String, String> authData) {
+        return given()
+                .filter(new AllureRestAssured())
+                .body(authData)
+                .put(url)
+                .andReturn();
+    }
+
+    @Step("Make a PUT-request with auth data")
+    public void makePutRequestAuth(String url, Map<String, String> editData, String header, String cookie) {
+        given()
+                .filter(new AllureRestAssured())
+                .header("x-csrf-token", header)
+                .cookie("auth_sid", cookie)
+                .body(editData)
+                .put(url)
+                .andReturn();
+    }
+
+    @Step("Create a new user and return id")
+    public String createNewUserAndReturnId(String url) {
+        Map<String, String> authData = DataGenerator.getRegistrationData();
+
+        JsonPath response = given()
+                .filter(new AllureRestAssured())
+                .body(authData)
+                .post(url)
+                .jsonPath();
+
+        return response.get("id");
+    }
+
+    @Step("Return user's auth data")
+    public Map<String, String> returnAuthDataLoggedUser(String url, Map<String, String> credential) {
+        Response response = given()
+                .filter(new AllureRestAssured())
+                .body(credential)
+                .post(url)
+                .andReturn();
+
+        String header = response.getHeader("x-csrf-token");
+        String cookie = response.getCookie("auth_sid");
+
+        Map<String, String> authData = new HashMap<>();
+        authData.put("header", header);
+        authData.put("cookie", cookie);
+
+        return authData;
     }
 }
--- src/test/java/tests/UserEditTest.java
+++ src/test/java/tests/UserEditTest.java
@@ -3,15 +3,31 @@ package tests;
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import lib.ApiCoreRequests;
 import lib.Assertions;
 import lib.BaseTestCase;
 import lib.DataGenerator;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Stream;
 
 public class UserEditTest extends BaseTestCase {
+    String loginUrl = "https://playground.learnqa.ru/api/user/login";
+    String getUserDataUrl = "https://playground.learnqa.ru/api/user/";
+    String registerUrl = "https://playground.learnqa.ru/api/user/";
+    private final ApiCoreRequests apiCoreRequests = new ApiCoreRequests();
+
+    private static Stream<Arguments> testData() {
+        return Stream.of(
+                Arguments.of("email", "@", "Email without '@'"),
+                Arguments.of("firstName", DataGenerator.getRandomStringByLength(1), "Very short name")
+        );
+    }
 
     @Test
     public void testEditJustCreatedTest() {
@@ -39,13 +55,11 @@ public class UserEditTest extends BaseTestCase {
         Map<String, String> editData = new HashMap<>();
         editData.put("firstName", newName);
 
-        Response responseEditUser = RestAssured
-                .given()
+        RestAssured.given()
                 .header("x-csrf-token", this.getHeader(responseGetAuth, "x-csrf-token"))
                 .cookie("auth_sid", this.getCookie(responseGetAuth, "auth_sid"))
                 .body(editData)
-                .put("https://playground.learnqa.ru/api/user/" + userId)
-                .andReturn();
+                .put("https://playground.learnqa.ru/api/user/" + userId);
 
         Response responseUserData = RestAssured
                 .given()
@@ -54,6 +68,91 @@ public class UserEditTest extends BaseTestCase {
                 .get("https://playground.learnqa.ru/api/user/" + userId)
                 .andReturn();
 
-        Assertions.asserJsonByName(responseUserData, "firstName", newName);
+        Assertions.assertJsonByName(responseUserData, "firstName", newName);
     }
+
+    @Test
+    public void testEditNotAuthUser() {
+        //Create a new user
+        Map<String, String> newUserData = DataGenerator.getRegistrationData();
+        JsonPath responseCreateUser = apiCoreRequests.makePostRequestJsonPath(registerUrl, newUserData);
+        String newUserId = responseCreateUser.get("id");
+
+        //Edit user data
+        String newName = "Changed Name";
+        Map<String, String> editData = new HashMap<>();
+        editData.put("firstName", newName);
+
+        Response response = apiCoreRequests.makePutRequest(getUserDataUrl + newUserId, editData);
+
+        Assertions.assertResponseCodeEquals(response, 400);
+        Assertions.assertResponseTextEquals(response, "Auth token not supplied");
+    }
+
+    @Test
+    public void testEditAnotherUser() {
+        //Create a new user 1
+        Map<String, String> newUser1Data = DataGenerator.getRegistrationData();
+        apiCoreRequests.makePostRequest(registerUrl, newUser1Data);
+
+        //Create a new user 2
+        String newUser2Id = apiCoreRequests.createNewUserAndReturnId(registerUrl);
+
+        //Login as user1 and get auth data
+        Map<String, String> userCredentials = new HashMap<>();
+        userCredentials.put("email", newUser1Data.get("email"));
+        userCredentials.put("password", newUser1Data.get("password"));
+
+        Response responseLoginUser = apiCoreRequests.makePostRequest(loginUrl, userCredentials);
+        String cookie = this.getCookie(responseLoginUser, "auth_sid");
+        String header = this.getHeader(responseLoginUser, "x-csrf-token");
+
+        //Edit user2 info using user1 auth data
+        String newName = "Changed Name";
+        Map<String, String> editData = new HashMap<>();
+        editData.put("firstName", newName);
+
+        apiCoreRequests.makePutRequestAuth
+                (getUserDataUrl + newUser2Id, editData, header, cookie);
+
+        //Get user2 info
+        Response responseGetEditedData = apiCoreRequests.makeGetRequest(getUserDataUrl + newUser2Id, header, cookie);
+
+        Assertions.assertJsonHasField(responseGetEditedData, "username");
+        Assertions.assertJsonHasNotField(responseGetEditedData, "firstName");
+        Assertions.assertJsonHasNotField(responseGetEditedData, "lastName");
+        Assertions.assertJsonHasNotField(responseGetEditedData, "email");
+    }
+
+    @ParameterizedTest(name = "{index} {2}")
+    @MethodSource("testData")
+    public void testSetInvalidDataAuthUser(String key, String value, String ignoredNameTest) {
+        //Create a new user
+        Map<String, String> newUserData = DataGenerator.getRegistrationData();
+        JsonPath responseCreate = apiCoreRequests.makePostRequestJsonPath(registerUrl, newUserData);
+        String userId = responseCreate.get("id");
+
+        //Login as user and get auth data
+        Map<String, String> userCredentials = new HashMap<>();
+        userCredentials.put("email", newUserData.get("email"));
+        userCredentials.put("password", newUserData.get("password"));
+
+        Response responseLoginUser = apiCoreRequests.makePostRequest(loginUrl, userCredentials);
+        String cookie = this.getCookie(responseLoginUser, "auth_sid");
+        String header = this.getHeader(responseLoginUser, "x-csrf-token");
+
+        //Edit user info, set invalid data
+        String newValue = userCredentials.replace(key, value);
+        Map<String, String> editData = new HashMap<>();
+        editData.put(key, newValue);
+
+        apiCoreRequests.makePutRequestAuth
+                (getUserDataUrl + userId, editData, header, cookie);
+
+        //Get user info
+        Response responseGetEditedData = apiCoreRequests.makeGetRequest(getUserDataUrl + userId, header, cookie);
+
+        Assertions.assertJsonByName(responseGetEditedData, key, newUserData.get(key));
+    }
+
 }
